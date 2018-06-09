2a9166bb1b3ae3f7b805b646021bc9530941b09b
sklprime <35468130+sklprime@users.noreply.github.com>
2018-06-09 13:56:00 +0300

    Update FileHandler.java

--- hw5/pkg1/FileHandler.java
+++ hw5/pkg1/FileHandler.java
@@ -5,10 +5,30 @@
  */
 package hw5.pkg1;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
 /**
  *
  * @author Администратор
  */
-public class FileHandler {
-    
+public class HW51 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FileFilter ffilt = new FileCopy("doc");
+        File folderFrom = new File("folderFrom");
+        File folderTo = new File("folderTo");
+        try {
+            FileHandler.folderCopy(folderFrom, folderTo, ffilt);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
