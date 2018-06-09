9d7171de2b6cb5b1596fce660b7a71432cd21f2c
sklprime <35468130+sklprime@users.noreply.github.com>
2018-06-09 13:58:03 +0300

    Update FileHandler.java

--- hw5/pkg1/FileHandler.java
+++ hw5/pkg1/FileHandler.java
@@ -7,28 +7,44 @@ package hw5.pkg1;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  *
  * @author Администратор
  */
-public class HW51 {
+public class FileHandler {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        FileFilter ffilt = new FileCopy("doc");
-        File folderFrom = new File("folderFrom");
-        File folderTo = new File("folderTo");
-        try {
-            FileHandler.folderCopy(folderFrom, folderTo, ffilt);
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+    public static void fileCopy(File from, File to) throws IOException {
+        if (from == null || to == null) {
+            throw new IllegalArgumentException("Файл не найден");
         }
+        try (InputStream fis = new FileInputStream(from);
+                OutputStream fos = new FileOutputStream(to)) {
+            byte[] buffer = new byte[1024 * 1024];
+            int readByte = 0;
+            for (; (readByte = fis.read(buffer)) > 0;) {
+                fos.write(buffer, 0, readByte);
+            }
+        } catch (IOException e) {
+            throw e;
+        }
+
     }
 
+    public static void folderCopy(File folderFrom, File folderTo,
+            FileFilter ffilt) throws IOException {
+        if (folderFrom == null || folderTo == null || ffilt == null) {
+            throw new IllegalArgumentException("Папки не существует");
+        }
+
+        File[] files = folderFrom.listFiles(ffilt);
+        for (File file : files) {
+            fileCopy(file, new File(folderTo, file.getName()));
+        }
+    }
 }
