e7d1e0843b697947a2f74ba117ec913fa1124213
sklprime <35468130+sklprime@users.noreply.github.com>
2018-06-09 13:58:43 +0300

    Update FileCopy.java

--- hw5/pkg1/FileCopy.java
+++ hw5/pkg1/FileCopy.java
@@ -3,12 +3,41 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package hw5.pkg1;
+
+import java.io.File;
+import java.io.FileFilter;
 
 /**
  *
  * @author Администратор
  */
-public class FileCopy {
-    
+public class FileCopy implements FileFilter {
+
+    private String[] arr;
+
+    public FileCopy(String... arr) {
+        super();
+        this.arr = arr;
+    }
+
+    private boolean check(String ext) {
+        for (String stringExt : arr) {
+            if (stringExt.equals(ext)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean accept(File pathname) {
+        int pointerIndex = pathname.getName().lastIndexOf(".");
+        if (pointerIndex == -1) {
+            return false;
+        }
+        String ext = pathname.getName().substring(pointerIndex + 1);
+        return check(ext);
+    }
 }
